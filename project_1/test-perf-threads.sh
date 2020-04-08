6428e91ec39569b4cf7ebd91c1df2539d7492cde
Saket Roy <roy05@live.missouristate.edu>
2020-04-08 18:25:24 +0530

    --fix: Error in bash script

--- project_1/test-perf-threads.sh
+++ project_1/test-perf-threads.sh
@@ -3,11 +3,11 @@
 # Number of trials
 for t in 1 5 10 50 100 500 1000 5000 10000 50000 100000 500000 1000000 5000000 10000000 50000000
 do 	
-	#Number of trials
+	#Number of threads
 	for th in $(seq 0 7)
 	do
 		g++ -DNUMT=$((2**$th)) -DNUMTRIALS=$t project-1.cpp -o perf-threads -lm -fopenmp
-		./prog
+		./perf-threads
 		printf "\t"
 	done
 	printf "\n"
--- project_1/test-perf-trials.sh
+++ project_1/test-perf-trials.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Number of threads
-for th in $(seq 0 7)
+for th in 1 2 4 6 8 10 12
 do 	
 	# Number of trials
 	for t in $(seq 0 7)
 	do
-		g++ -DNUMT=$((2**$th)) -DNUMTRIALS=$((10**$t)) project-1.cpp -o perf-trials -lm -fopenmp
-		./prog
+		g++ -DNUMT=$th -DNUMTRIALS=$((10**$t)) project-1.cpp -o perf-trials -lm -fopenmp
+		./perf-trials
 		printf "\t"
 	done
 	printf "\n"
