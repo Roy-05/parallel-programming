b2241d427d7c351bf0a62e6d78fb098abc43d2b5
Saket Roy <roy05@live.missouristate.edu>
2020-04-22 23:25:17 +0530

    Add initial openMP code to integrate hyperquadratic

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -1,26 +1,52 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+//Set the number of threads
+#ifndef NUMT
+#define NUMT		1
+#endif
+
+//Set the number of nodes
+#ifndef NUMNODES 	
+#define NUMNODES	1000
+#endif
 
 #define XMIN     -1.
 #define XMAX      1.
 #define YMIN     -1.
 #define YMAX      1.
 
+//Function Prototype
 float Height( int, int );
 
 int main( int argc, char *argv[ ] )
-{
-	/*
-	. . .
+{	
+	//Verify OpenMP support
+	#ifndef _OPENMP
+		fprintf( stderr, "No OpenMP support!\n" );
+		return 1;
+	#endif
+
+	//Set the number of threads for the for loop
+	omp_set_num_threads(NUMT);
+
 
 	// the area of a single full-sized tile:
-
 	float fullTileArea = (  ( ( XMAX - XMIN )/(float)(NUMNODES-1) )  *
 				( ( YMAX - YMIN )/(float)(NUMNODES-1) )  );
 
 	// sum up the weighted heights into the variable "volume"
 	// using an OpenMP for loop and a reduction:
-
-	?????
-	*/
+	#pragma omp parallel for default(none) reduction(+:volume)
+	for( int i = 0; i < NUMNODES*NUMNODES; i++ )
+	{
+		int iu = i % NUMNODES;
+		int iv = i / NUMNODES;
+		float z = Height( iu, iv );
+	}
 }
 
 float
