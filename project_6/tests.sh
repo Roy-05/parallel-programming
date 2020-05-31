3f03b1868c7ef961ca7a04e4de6c905a7199a1e5
Saket Roy <roy05@live.missouristate.edu>
2020-05-31 13:07:20 +0530

    OpenCL Mult + Mult/Add complete

--- project_6/tests.sh
+++ project_6/tests.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-for gws in $(seq 10 2 26)
+# Empty the csv file on each execution
+> "data.txt"
+
+# Generate labels for data
+for lbl in $(seq 3 9)
 do
+	printf "\t"$((2**$lbl)) >> "data.txt"
+done
+
+printf "\n" >> "data.txt"
+
+# Compile and execute OpenCL code
+for gws in $(seq 10 2 24)
+do
+	printf $((2**$gws))"\t" >> "data.txt"
 	for lws in $(seq 3 9)
 	do
 		g++ -o first first.cpp -DNUM_ELEMENTS=$((2**$gws)) -DLOCAL_SIZE=$((2**$lws)) -lOpenCL -lm -fopenmp -w
 		./first
+		printf "\t" >> "data.txt"
 	done
+	printf "\n" >> "data.txt"
 done
 
 
